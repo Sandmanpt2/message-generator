a5b553c6cf034faf354298afe412b6b16913bec4
Sandmanpt2 <88346596+Sandmanpt2@users.noreply.github.com>
2021-08-05 19:16:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const authors = [
+    'Gandhi' ,
+    'Martin Luther King' ,
+    'Steve Irwin' ,
+    'Winston Churchill' ,
+    'George Orwell' ,
+    'Genghis Khan' ,
+    'Nero' ,
+    'Bob Ross' ,
+    'Da Vinci' ,
+    'Marie Antoinette'
+]
